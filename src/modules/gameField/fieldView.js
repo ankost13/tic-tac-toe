1d8462d87d4a06bf2bd02e8083921319e8082b58
Anastasiyka <anastasyam1908@gmail.com>
2024-12-29 17:57:54 +0200

    Add interactive square

--- src/modules/gameField/fieldView.js
+++ src/modules/gameField/fieldView.js
@@ -1,11 +1,15 @@
 import {View} from "../../utils/view";
-import {Assets, Sprite} from "pixi.js";
+import {Assets, Container, Graphics, Sprite} from "pixi.js";
 
 export class FieldView extends View {
     constructor(parent) {
         super(parent);
         this.createFieldSprite();
         this.position.set(window.innerWidth / 2, window.innerHeight / 2);
+        this.createInteractiveSquare();
+
+        this.setInteractiveSquare(true);
+        this.addEventSquare();
     }
 
     createFieldSprite() {
@@ -20,6 +24,42 @@ export class FieldView extends View {
             scale: .6,
         })
         this.addChild(this.field);
+    }
+
+    createInteractiveSquare() {
+        const parentForSquare = new Container();
+        this.addChild(parentForSquare);
+        const squareWidth = 180;
+        const squareHeight = 180;
+        this.collectionSquare = [];
+        for (let i = 0; i < 9; i++) {
+            const square = new Graphics();
+            square.rect(0, 0, squareWidth, squareHeight);
+            square.x = (i % 3) * 185 - 185;
+            square.y = parseInt(i / 3 + "") * 185 - 185;
+            square.pivot.set(90, 90);
+            square.fill(0xde3249);
+            parentForSquare.addChild(square);
+            square.inUsed = false;
+            this.collectionSquare.push(square);
+        }
+    }
+
+    setInteractiveSquare(on) {
+        this.collectionSquare.forEach((square) => {
+            square.interactive = !square.inUsed && on;
+            square.cursor = "pointer";
+        })
+    }
+
+    addEventSquare() {
+        this.collectionSquare.forEach((square) => {
+            if (!square.inUsed) {
+                square.on("pointerup", () => {
+
+                })
+            }
+        })
     }
 }
 
